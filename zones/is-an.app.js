529f6f4f4130a51d5336d0b4653c6e9ef07482ce
a-wlj-ddns-ip-net <aaa@wlj.ddns-ip.net>
2025-01-19 19:59:41 +0800

    Update is-an.app.js

--- zones/is-an.app.js
+++ zones/is-an.app.js
@@ -49,4 +49,5 @@ D('is-an.app', NewRegistrar('none'), DnsProvider(NewDnsProvider('cloudflare')), 
   CNAME('smp', 'yandao0313.github.io.'),
   CNAME('snapscript', 'cname.vercel-dns.com.'),
   CNAME('yt2mp3', 'cname.vercel-dns.com.'),
+  CNAME('wlj1993', 'cname.vercel-dns.com.'),
 END)
